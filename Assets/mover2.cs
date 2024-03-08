94efbeecd3c4dcc8680f1b51c432ebc51b961e16
AvivShevach <92975487+AvivShevach@users.noreply.github.com>
2024-03-08 11:44:46 +0200

    Update mover2.cs

--- Assets/mover2.cs
+++ Assets/mover2.cs
@@ -3,29 +3,31 @@ using UnityEngine;
 public class MoverByClickWASD : MonoBehaviour
 {
     public float speed = 10f;
+    public float dist = 0.7f;
+    public float zeroMove = 0;
 
     private void Update()
     {
-        if (Vector3.Distance(transform.position, GameObject.Find("player1").transform.position) < 0.7f)
+        if (Vector3.Distance(transform.position, GameObject.Find("player1").transform.position) < dist)
         {
             Destroy(GameObject.Find("player2"));
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += new Vector3(speed * Time.deltaTime, 0f, 0f);
+            transform.position += new Vector3(speed * Time.deltaTime, zeroMove, zeroMove);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position -= new Vector3(speed * Time.deltaTime, 0f, 0f);
+            transform.position -= new Vector3(speed * Time.deltaTime, zeroMove, zeroMove);
         }
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += new Vector3(0f, speed * Time.deltaTime, 0f);
+            transform.position += new Vector3(zeroMove, speed * Time.deltaTime, zeroMove);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position -= new Vector3(0f, speed * Time.deltaTime, 0f);
+            transform.position -= new Vector3(zeroMove, speed * Time.deltaTime, zeroMove);
         }
     }
 }
