917dd3c6a4cf38db490be8cbe759a45e7f2e646d
AvivShevach <92975487+AvivShevach@users.noreply.github.com>
2024-03-08 11:44:21 +0200

    Update mover1.cs

--- Assets/mover1.cs
+++ Assets/mover1.cs
@@ -3,25 +3,26 @@ using UnityEngine;
 public class MoverByClick : MonoBehaviour
 {
     public float speed = 10f;
+    public float zeroMove = 0f;
 
     private void Update()
     {
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position += new Vector3(speed * Time.deltaTime, 0f, 0f);
+            transform.position += new Vector3(speed * Time.deltaTime, zeroMove, zeroMove);
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position -= new Vector3(speed * Time.deltaTime, 0f, 0f);
+            transform.position -= new Vector3(speed * Time.deltaTime, zeroMove, zeroMove);
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.position += new Vector3(0f, speed * Time.deltaTime, 0f);
+            transform.position += new Vector3(0f, speed * Time.deltaTime, zeroMove);
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.position -= new Vector3(0f, speed * Time.deltaTime, 0f);
+            transform.position -= new Vector3(zeroMove, speed * Time.deltaTime, zeroMove);
         }
     }
 }
